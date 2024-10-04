9757e22b273785a7e873d58f49bb8cde33178c66
boracingoz26 <boracingoz@gmail.com>
2024-10-04 09:41:09 +0300

    FloorController added

---
+++ Assets/Scritps/Controller/FloorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Controller
+{
+    public class FloorController : MonoBehaviour
+    {
+        Material _material;
+        [Range(1,2.2f)]
+        [SerializeField]private float _moveSpeed = 1;
+
+        private void Awake()
+        {
+            _material = GetComponentInChildren<MeshRenderer>().material;
+        }
+
+        private void Update()
+        {
+            _material.mainTextureOffset += Vector2.down * Time.deltaTime * _moveSpeed;
+        }
+    }
+}
