ed9918d9a68e5d6497d254b60fd61f690800322a
boracingoz26 <boracingoz@gmail.com>
2024-10-04 10:46:33 +0300

    Implemented enemy movement system with controller and movement logic

---
+++ Assets/Scritps/Controller/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using Movements;
+using System.Collections;
+using UnityEngine;
+
+namespace Controller
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] float _moveSpeed;
+        EnemyMovement _enemyMovement;
+
+        public float MoveSpeed => _moveSpeed;
+
+        private void Awake()
+        {
+            _enemyMovement = new EnemyMovement(this);
+        }
+
+        private void FixedUpdate()
+        {
+            _enemyMovement.FixedTick();
+        }
+
+    }
+}
---
+++ Assets/Scritps/Movements/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using Controller;
+using System.Collections;
+using UnityEngine;
+
+namespace Movements
+{
+    public class EnemyMovement
+    {
+        EnemyController _enemyController;
+        private float _moveSpeed;
+
+        public EnemyMovement(EnemyController enemyController)
+        {
+            _enemyController = enemyController;
+            _moveSpeed = _enemyController.MoveSpeed;    
+        }
+
+        public void FixedTick(float veritcal = 1)
+        {
+            _enemyController.transform.Translate(Vector3.back * veritcal * _moveSpeed * Time.deltaTime);
+        }
+    }
+}
