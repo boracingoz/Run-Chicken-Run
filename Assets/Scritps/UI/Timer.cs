baa8197dc463a7a754ac7ae1f2ac6c2d3973b8a7
boracingoz26 <boracingoz@gmail.com>
2024-10-06 11:52:36 +0300

    Added Timer logic.

---
+++ Assets/Scritps/UI/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace UI
+{
+    public class Timer : MonoBehaviour
+    {
+        private TMP_Text _text;
+        private float _curTime;
+
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            _curTime += Time.deltaTime;
+            _text.text = ((int)_curTime).ToString();
+        }
+    }
+}
