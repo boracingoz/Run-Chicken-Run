0b8051e5acf2f25a68dfb755997470039c14c9ae
boracingoz26 <boracingoz@gmail.com>
2024-10-08 14:26:41 +0300

    Added restricted player horizontal movement logic in HoriMover.

--- Assets/Scritps/Movements/HoriMover.cs
+++ Assets/Scritps/Movements/HoriMover.cs
@@ -2,45 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Controllers;
 using UnityEngine;
+
 namespace Movements
 {
     public class HoriMover
     {
         PlayerController _playerController;
+        private const float MIN_X = -3.05f;
+        private const float MAX_X = 3.05f;
+
         public HoriMover(PlayerController playerController)
         {
             _playerController = playerController;
         }
+
         public void TickFixed(float horizontal, float moveSpeed)
         {
             if (horizontal == 0f) return;
 
-            _playerController.transform.Translate(Vector3.right * horizontal * Time.deltaTime * moveSpeed);
+            Vector3 movement = Vector3.right * horizontal * Time.deltaTime * moveSpeed;
+            Vector3 newPosition = _playerController.transform.position + movement;
+
+            // X pozisyonunu sýnýrlar içinde tutuyoruz
+            newPosition.x = Mathf.Clamp(newPosition.x, MIN_X, MAX_X);
+
+            _playerController.transform.position = newPosition;
         }
     }
 }
-
-#region Yedek Mover
-//using System.Collections;
-//using System.Collections.Generic;
-//using Controllers;
-//using UnityEngine;
-
-//namespace Movements
-//{
-//    public class HorizontalMover
-//    {
-//        PlayerController _playerController;
-//        public HorizontalMover(PlayerController playerController)
-//        {
-//            _playerController = playerController;
-//        }
-//        public void TickFixed(float horizontal, float moveSpeed)
-//        {
-//            if (horizontal == 0f) return;
-
-//            _playerController.transform.Translate(Vector3.right * horizontal * Time.deltaTime * moveSpeed);
-//        }
-//    }
-//}
-#endregion
